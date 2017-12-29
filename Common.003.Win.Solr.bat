c823af11eb2b533507f572eeecf1beaa7c39502b
HY-ZhengWei <hy.zhengwei@qq.com>
2017-12-29 16:47:41 +0800

    1.1.1
    Signed-off-by: HY-ZhengWei <hy.zhengwei@qq.com>

--- Common.003.Win.Solr.bat
+++ Common.003.Win.Solr.bat
@@ -4,7 +4,7 @@ cd .\bin
 
 rd /s/q .\org\hy\common\solr\junit
 
-jar cvfm hy.common.solr.jar MANIFEST.MF LICENSE org
+jar cvfm hy.common.solr.jar MANIFEST.MF META-INF org
 
 copy hy.common.solr.jar ..
 del /q hy.common.solr.jar
--- Common.004.Mac.Solr.sh
+++ Common.004.Mac.Solr.sh
@@ -4,7 +4,7 @@ cd ./bin
 
 rm -R ./org/hy/common/solr/junit
 
-jar cvfm hy.common.solr.jar MANIFEST.MF LICENSE org
+jar cvfm hy.common.solr.jar MANIFEST.MF META-INF org
 
 cp hy.common.solr.jar ..
 rm hy.common.solr.jar
