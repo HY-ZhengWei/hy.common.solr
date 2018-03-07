0ba970c61ff40a41721f66a261d1f508aacfeebc
HY-ZhengWei <hy.zhengwei@qq.com>
2018-03-07 12:09:46 +0800

    1.1.2

--- Common.003.Win.Solr.bat
+++ Common.003.Win.Solr.bat
@@ -2,7 +2,9 @@
 
 cd .\bin
 
+
 rd /s/q .\org\hy\common\solr\junit
+
 
 jar cvfm hy.common.solr.jar MANIFEST.MF META-INF org
 
@@ -10,3 +12,12 @@ copy hy.common.solr.jar ..
 del /q hy.common.solr.jar
 cd ..
 
+
+
+
+
+cd .\src
+jar cvfm hy.common.solr-sources.jar MANIFEST.MF META-INF org 
+copy hy.common.solr-sources.jar ..
+del /q hy.common.solr-sources.jar
+cd ..
--- Common.004.Mac.Solr.sh
+++ Common.004.Mac.Solr.sh
@@ -2,10 +2,22 @@
 
 cd ./bin
 
+
 rm -R ./org/hy/common/solr/junit
+
 
 jar cvfm hy.common.solr.jar MANIFEST.MF META-INF org
 
 cp hy.common.solr.jar ..
 rm hy.common.solr.jar
+cd ..
+
+
+
+
+
+cd ./src
+jar cvfm hy.common.solr-sources.jar MANIFEST.MF META-INF org 
+cp hy.common.solr-sources.jar ..
+rm hy.common.solr-sources.jar
 cd ..
